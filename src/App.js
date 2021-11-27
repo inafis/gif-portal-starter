b10833410859bd0d74fe5c4693ac88c5b20210a4
Khalil Brown <khalilbrown@khalils-air.myfiosgateway.com>
2021-11-26 22:04:05 -0500

    env vars

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
 import {Program, Provider, web3} from '@project-serum/anchor';
 
 import idl from './idl.json';
-import kp from './keypair.json'
 
 const {SystemProgram, Keypair} = web3;
 
@@ -14,8 +13,6 @@ let key;
 if(process.env.REACT_APP_key){
   const data = JSON.parse(process.env.REACT_APP_key)
   key = Object.values(data.secretKey)
-}else{
-  key = Object.values(kp._keypair.secretKey)
 }
 const arr = key;
 const secret = new Uint8Array(arr)
